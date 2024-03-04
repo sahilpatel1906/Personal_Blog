fb642427edb7a3b69f83787de34fe9bcafbfc0c9
Saba Kia <skia6589@gmail.com>
2024-03-04 16:17:44 +0000

    changes

--- blog_api/src/main/java/com/example/blog_api/services/UserService.java
+++ blog_api/src/main/java/com/example/blog_api/services/UserService.java
@@ -1,4 +1,15 @@
 package com.example.blog_api.services;
 
+import com.example.blog_api.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+
 }
