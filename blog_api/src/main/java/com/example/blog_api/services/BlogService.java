9c37cb21f25c4eb8510ffe8391fcf38246f66508
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-04 15:37:33 +0000

    Added code to BlogRepository and Blog Service.

--- blog_api/src/main/java/com/example/blog_api/services/BlogService.java
+++ blog_api/src/main/java/com/example/blog_api/services/BlogService.java
@@ -1,4 +1,12 @@
 package com.example.blog_api.services;
 
+import com.example.blog_api.repositories.BlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class BlogService {
+
+    @Autowired
+    BlogRepository blogRepository;
 }
