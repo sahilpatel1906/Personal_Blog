2379b28f464335e9ccb0019dfb4080337cb163e9
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-04 15:50:23 +0000

    Add get mapping for blogs.

--- blog_api/src/main/java/com/example/blog_api/controllers/BlogController.java
+++ blog_api/src/main/java/com/example/blog_api/controllers/BlogController.java
@@ -1,4 +1,26 @@
 package com.example.blog_api.controllers;
 
+import com.example.blog_api.models.Blog;
+import com.example.blog_api.services.BlogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/blogs")
 public class BlogController {
+
+    @Autowired
+    BlogService blogService;
+
+    @GetMapping
+    public ResponseEntity<List<Blog>> getAllBlogs(){
+        List<Blog> blogs = blogService.getAllBlogs();
+        return new ResponseEntity<>(blogs, HttpStatus.OK);
+    }
 }
--- blog_api/src/main/java/com/example/blog_api/services/BlogService.java
+++ blog_api/src/main/java/com/example/blog_api/services/BlogService.java
@@ -1,12 +1,19 @@
 package com.example.blog_api.services;
 
+import com.example.blog_api.models.Blog;
 import com.example.blog_api.repositories.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class BlogService {
 
     @Autowired
     BlogRepository blogRepository;
+
+    public List<Blog> getAllBlogs() {
+        return blogRepository.findAll();
+    }
 }
