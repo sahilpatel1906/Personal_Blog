5ab07d3dc2715e78047dc40d4bdaa03cc08fa8a8
Karen Yip <karenyip1999@gmail.com>
2024-03-05 10:04:23 +0000

    Added getAllComments() in CommentController

--- blog_api/src/main/java/com/example/blog_api/controllers/CommentController.java
+++ blog_api/src/main/java/com/example/blog_api/controllers/CommentController.java
@@ -1,4 +1,33 @@
 package com.example.blog_api.controllers;
 
+import com.example.blog_api.models.Comment;
+import com.example.blog_api.services.CommentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/comments")
 public class CommentController {
+
+    @Autowired
+    CommentService commentService;
+    @GetMapping
+    public ResponseEntity<List<Comment>>getAllComments()
+    {
+        List<Comment>comments;
+        comments = commentService.getAllComments();
+        return new ResponseEntity<>(comments, HttpStatus.OK);
+    }
+
+
+
+
+
 }
