62d6d170edf9eebfde66e2a9f36734a08310bc7b
Tadiwanashe Dzvoti <tadiwadzvoti@hotmail.com>
2024-03-07 09:58:57 +0000

    Added some more posts to the blogs.

--- blog_api/src/main/java/com/example/blog_api/components/DataLoader.java
+++ blog_api/src/main/java/com/example/blog_api/components/DataLoader.java
@@ -83,9 +83,18 @@ public class DataLoader implements ApplicationRunner {
         Post post1 = new Post("Weight Loss Journey", fitnessBlog, "15/05/24", "My 5 Workout Routines to lose Weight!", null);
         Post post2 = new Post("Travel places",travelBlog, "10/01/24", "My 5 Fav places to Travel", "https://static.independent.co.uk/2023/08/02/10/iStock-507489863.jpg");
         Post post3 = new Post("Best places",travelBlog, "06/02/24", "Most expensive places to visit", null);
-        Post post4 = new Post("Fashionable",fashionBlog, "09/11/24", "The Smart Casual Look", "image 1");
-        Post post5 = new Post("Book Review",bookReviewBlog, "09/11/24", "My Top 10 books of 2023", "image 1");
-        Post post6 = new Post("My Diary",diaryBlog, "09/11/24", "Diary entry 103", "image 1");
+        Post post4 = new Post("Fashionable",fashionBlog, "09/11/24", "The Smart Casual Look", null);
+        Post post5 = new Post("Book Review",bookReviewBlog, "09/11/24", "My Top 10 books of 2023", null);
+        Post post6 = new Post("My Diary",diaryBlog, "09/11/24", "Diary entry 103", null);
+        Post post7 = new Post("Healthy Habits", healthBlog, "20/02/24", "5 Tips for a Healthier Lifestyle", "https://example.com/image7.jpg");
+        Post post8 = new Post("Football Tactics", footballBlog, "22/03/24", "Analyzing the Latest Strategies", "https://example.com/image8.jpg");
+        Post post9 = new Post("Movie Recommendations", movieBlog, "05/10/22", "Must-Watch Films of the Year", null);
+        Post post10 = new Post("Comic Review", comicBlog, "18/11/24", "Exploring the Latest Comic Releases", "https://example.com/image9.jpg");
+        Post post11 = new Post("Favorite Fashion Trends", fashionBlog, "03/02/24", "Trendy Outfit Ideas for the Season", "https://example.com/image11.jpg");
+        Post post12 = new Post("Fitness Tips", fitnessBlog, "28/05/24", "Effective Exercises for Building Muscle", "https://example.com/image12.jpg");
+        Post post13 = new Post("Adventure Stories", travelBlog, "15/03/24", "Exciting Tales from Around the Globe", "https://example.com/image13.jpg");
+        Post post14 = new Post("Book Recommendation", bookReviewBlog, "30/11/24", "A Must-Read Novel for Bookworms", "https://example.com/image14.jpg");
+        Post post15 = new Post("Reflecting on Life", diaryBlog, "02/12/24", "Thoughts on Recent Experiences", "https://example.com/image15.jpg");
 
         postRepository.save(post1);
         postRepository.save(post2);
@@ -93,6 +102,15 @@ public class DataLoader implements ApplicationRunner {
         postRepository.save(post4);
         postRepository.save(post5);
         postRepository.save(post6);
+        postRepository.save(post7);
+        postRepository.save(post8);
+        postRepository.save(post9);
+        postRepository.save(post10);
+        postRepository.save(post11);
+        postRepository.save(post12);
+        postRepository.save(post13);
+        postRepository.save(post14);
+        postRepository.save(post15);
 
         Comment fitnessComment = new Comment(post1, "Great will try this out", false, user1.getName());
         Comment fitnessComment1 = new Comment(post1, "Boring ...", false, user2.getName());
